b49a3f58c07c5cd615fcca8c5a0ee77a8dc2284d
NeoMixlax <nicolas.ramirezc@usantoto.edu.co>
2023-05-02 15:11:51 -0500

    Adding manual feature for nvim-tree autoclosing

--- nvim/lua/NeoMixlax/autocmd.lua
+++ nvim/lua/NeoMixlax/autocmd.lua
@@ -47,3 +47,13 @@ A.nvim_create_autocmd('BufReadPost', {
         end
     end,
 })
+
+-- Enabling autoclose for nvim-tree
+A.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and require("nvim-tree.utils").is_nvim_tree_buf() then
+      vim.cmd "quit"
+    end
+  end
+})
